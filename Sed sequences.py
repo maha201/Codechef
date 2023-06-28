ab4dba21d90d766a935537447799b5d360db8e90
R. Mahadevan <97651598+maha201@users.noreply.github.com>
2023-06-28 07:15:01 +0530

    Sed sequences.py

---
+++ Sed sequences.py
@@ -0,0 +1,11 @@
+def findMinOpration(arr, k):
+    if sum(arr) % k == 0:
+        return 0
+    return 1
+
+
+t = int(input())
+for i in range(t):
+    n, k = map(int, input().split())
+    arr = list(map(int, input().split()))
+    print(findMinOpration(arr, k))
